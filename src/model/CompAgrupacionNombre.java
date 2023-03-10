62692a7d6f8a53e42577c0e17eb3f1326d1b75ec
Alvaro-Perez-Infante <alvaroperezin@gmail.com>
2023-03-10 09:35:06 +0100

    Push 10
    Clase CompAgrupacionOfiPnts documentada y refactorizada.

--- src/model/CompAgrupacionNombre.java
+++ src/model/CompAgrupacionNombre.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 /**
  * <h2>Clase Comparator de Agrupacion.</h2>
  * 
- * @version 9.0
+ * @version 10.0
  * @author &Aacute;lvaro P&eacute;rez Infante
  */
 public class CompAgrupacionNombre implements Comparator<Agrupacion> {
--- src/model/CompAgrupacionOfiPnts.java
+++ src/model/CompAgrupacionOfiPnts.java
@@ -2,13 +2,23 @@ package model;
 
 import java.util.Comparator;
 
-public class CompAgrupacionOfiPnts implements Comparator<AgrupacionOficial>{
+/**
+ * <h2>Clase Comparator de Agrupacion.</h2>
+ * 
+ * @version 10.0
+ * @author &Aacute;lvaro P&eacute;rez Infante
+ */
+public class CompAgrupacionOfiPnts implements Comparator<AgrupacionOficial> {
 
+	/**
+	 * M&eacute;metodo que usamos para comprar dos agrupaciones distintas en
+	 * funci&oacute;n a los puntos obtenidos en la calificaci&oacute;n de ambas.
+	 */
 	@Override
 	public int compare(AgrupacionOficial o1, AgrupacionOficial o2) {
 		int compare = 0;
-		
-		if(o1 != null && o2 != null) {
+
+		if (o1 != null && o2 != null) {
 			compare = o1.getPuntosObtenidos().compareTo(o2.getPuntosObtenidos());
 		}
 		return compare;
